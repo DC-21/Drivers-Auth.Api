08808233abb7eedaca6131f63bf84ea78a452291
DC-21 <cholahkuboko@gmail.com>
2023-10-09 21:33:58 +0200

    first commit

--- Data/ApiDbContext.cs
+++ Data/ApiDbContext.cs
@@ -1,8 +1,9 @@
 using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DriverAuth.Api.Data;
-public class ApiDbContext: DbContext
+public class ApiDbContext: IdentityDbContext
 {
     public ApiDbContext(DbContextOptions<ApiDbContext> options)
     :base(options)
