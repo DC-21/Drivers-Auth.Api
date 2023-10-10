bc0aab514bbaaaeaa180fcb8a31814b4f5b51f41
DC-21 <cholahkuboko@gmail.com>
2023-10-10 13:54:36 +0200

    first commit

--- Controllers/AuthManagementController.cs
+++ Controllers/AuthManagementController.cs
@@ -1,4 +1,7 @@
+using DriverAuth.Api.Configurations;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace DriverAuth.Api.Controllers;
 
@@ -7,9 +10,13 @@ namespace DriverAuth.Api.Controllers;
 public class AuthManagementController: ControllerBase
 {
     private readonly ILogger<AuthManagementController> _logger;
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly JwtConfig _jwtConfig;
 
-    public AuthManagementController(ILogger<AuthManagementController> logger)
+    public AuthManagementController(ILogger<AuthManagementController> logger, UserManager<IdentityUser> userManager, IOptionsMonitor<JwtConfig>optionsMonitor)
     {
         _logger = logger;
+        _userManager = userManager;
+        _jwtConfig = optionsMonitor.CurrentValue;
     }
 }
