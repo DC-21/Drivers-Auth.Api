d3cc769d79b66839750aae531c2802a9b3fcb88e
DC-21 <cholahkuboko@gmail.com>
2023-10-10 07:05:11 +0200

    first commit

---
+++ Models/DTOs/UserLoginRequestDto.cs
@@ -0,0 +1,7 @@
+namespace DriverAuth.Api.Models.DTOs;
+
+public class UserLoginRequestDto
+{
+    public string Email{get;set;}=string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
